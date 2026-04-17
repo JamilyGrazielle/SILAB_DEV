eb65934d8eb3334c3b585c1487ed2612a647c7b4
Laer-tty <emaildolaertty@gmail.com>
2026-04-17 16:03:47 -0300

    Adicionando projeto springboot

---
+++ backend/src/main/java/com/ifma/silab/SilabApplication.java
@@ -0,0 +1,13 @@
+package com.ifma.silab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SilabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SilabApplication.class, args);
+	}
+
+}
