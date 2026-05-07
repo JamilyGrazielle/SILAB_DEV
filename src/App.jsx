2f7b30bb2935e7c31821f6f9b589d9c582b6c594
rafaelgnv <rafaelreynan06@gmail.com>
2026-05-07 11:49:35 -0300

    Implementação da Tela de Reservas

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,8 @@ import Cadastro from './pages/Cadastro/Cadastro';
 import Dashboard from './pages/Dashboard/Dashboard';
 import Laboratorios from './pages/Laboratorios/Laboratorios';
 import Manutencoes from './pages/Manutencoes/Manutencoes';
-import Layout from './components/Layout/Layout'; // Importamos o nosso novo Layout
+import Layout from './components/Layout/Layout';
+import Reservas from './pages/Reservas/Reservas';
 
 export default function App() {
   return (
@@ -15,18 +16,18 @@ export default function App() {
         <Route path="/" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
 
-        {/* Rotas Privadas (Envolvidas pelo Layout) */}
+        {/* Rotas Protegidas - Envolvidas pelo Layout */}
         <Route element={<Layout />}>
           <Route path="/dashboard" element={<Dashboard />} />
           
-          {/* AQUI ESTÁ A CORREÇÃO: Adicionamos a rota dos Laboratórios */}
+          
           <Route path="/laboratorios" element={<Laboratorios />} />
           
-          {/* Já deixei a rota de manutenções preparada para o futuro */}
+          
           <Route path="/manutencoes" element={<Manutencoes />} />
           
-          {/* Futuras telas entrarão aqui dentro também! */}
-          {/* <Route path="/reservas" element={<Reservas />} /> */}
+          
+          <Route path="/reservas" element={<Reservas />} />
         </Route>
       </Routes>
     </BrowserRouter>
